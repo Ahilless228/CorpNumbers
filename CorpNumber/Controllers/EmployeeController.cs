8f0e8a3ad5309969af1124bd725ae17f4c73f307
Ahilless228 <alisherpro100gamer@gmail.com>
2025-06-30 16:54:27 +0600

    добавил инфо окно и пофиксил его

--- CorpNumber/Controllers/EmployeeController.cs
+++ CorpNumber/Controllers/EmployeeController.cs
@@ -39,5 +39,48 @@ namespace CorpNumber.Controllers
 
             return View("EmployeeIndex", employees);
         }
+
+        //выбор сотрудника из таблицы
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeDetails(int id)
+        {
+            var emp = await _context.Employees
+                .Include(e => e.PostNavigation)
+                .Include(e => e.DepartmentNavigation)
+                .Include(e => e.SectionNavigation)
+                .FirstOrDefaultAsync(e => e.CodeEmployee == id);
+
+            if (emp == null)
+                return NotFound();
+
+            string tabnumStr = emp.TabNum?.ToString("D5");
+            string photoFileName = $"{tabnumStr}.jpg";
+
+            string rootPath = Directory.GetCurrentDirectory();
+            string mainPath = Path.Combine(rootPath, "wwwroot", "Photo", photoFileName);
+            string archivePath = Path.Combine(rootPath, "wwwroot", "Photo", "Archive", photoFileName);
+
+            string webPhotoPath;
+            if (System.IO.File.Exists(mainPath))
+                webPhotoPath = Url.Content($"/Photo/{photoFileName}");
+            else if (System.IO.File.Exists(archivePath))
+                webPhotoPath = Url.Content($"/Photo/Archive/{photoFileName}");
+            else
+                webPhotoPath = Url.Content("~/images/default-profile.jpg");
+
+            return Json(new
+            {
+                fullname = $"{emp.Surname} {emp.Firstname} {emp.Midname}",
+                nameCh = emp.NameCh,
+                tabnum = tabnumStr,
+                inputDate = emp.InputDate?.ToString("dd.MM.yyyy"),
+                post = emp.PostNavigation?.Postt + " " + emp.PostNavigation?.PostCh,
+                department = emp.DepartmentNavigation?.DepartmentName + " " + emp.DepartmentNavigation?.DepartmentCh,
+                section = emp.SectionNavigation?.SectionName + " " + emp.SectionNavigation?.SectionCh,
+                photoPath = webPhotoPath
+            });
+        }
+
+
     }
 }
