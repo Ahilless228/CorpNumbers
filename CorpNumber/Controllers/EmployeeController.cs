23d26ced4af927a9f26008572ee2a819a750c0fc
Ahilless228 <alisherpro100gamer@gmail.com>
2025-07-01 11:27:36 +0600

    Added modal window

--- CorpNumber/Controllers/EmployeeController.cs
+++ CorpNumber/Controllers/EmployeeController.cs
@@ -40,7 +40,7 @@ namespace CorpNumber.Controllers
             return View("EmployeeIndex", employees);
         }
 
-        //выбор сотрудника из таблицы
+        //выбор сотрудника из таблицы, вся страница
         [HttpGet]
         public async Task<IActionResult> GetEmployeeDetails(int id)
         {
@@ -80,6 +80,69 @@ namespace CorpNumber.Controllers
                 photoPath = webPhotoPath
             });
         }
+
+        // Получение полной информации о сотруднике по ID в мод окне
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeFullDetails(int id)
+        {
+            var emp = await _context.Employees
+                .Include(e => e.PostNavigation)
+                .Include(e => e.DepartmentNavigation)
+                .Include(e => e.SectionNavigation)
+                .FirstOrDefaultAsync(e => e.CodeEmployee == id);
+
+            if (emp == null)
+                return NotFound();
+
+            string tabnumStr = emp.TabNum?.ToString("D5");
+            string photoFileName = $"{tabnumStr}.jpg";
+
+            string rootPath = Directory.GetCurrentDirectory();
+            string mainPath = Path.Combine(rootPath, "wwwroot", "Photo", photoFileName);
+            string archivePath = Path.Combine(rootPath, "wwwroot", "Photo", "Archive", photoFileName);
+
+            string webPhotoPath;
+            if (System.IO.File.Exists(mainPath))
+                webPhotoPath = Url.Content($"/Photo/{photoFileName}");
+            else if (System.IO.File.Exists(archivePath))
+                webPhotoPath = Url.Content($"/Photo/Archive/{photoFileName}");
+            else
+                webPhotoPath = Url.Content("~/images/default-profile.jpg");
+
+            return Json(new
+            {
+                emp.CodeEmployee,
+                emp.Surname,
+                emp.Firstname,
+                emp.Midname,
+                emp.NameCh,
+                emp.TabNum,
+                emp.InputDate,
+                emp.PartTime,
+                emp.ContractNumber,
+                emp.ContractDate,
+                emp.Fired,
+                emp.FiringDate,
+                emp.Hazard,
+                emp.Birthday,
+                emp.Passport,
+                emp.Address,
+
+                Post = emp.PostNavigation != null ? emp.PostNavigation.Postt + " " + emp.PostNavigation.PostCh : null,
+                Department = emp.DepartmentNavigation != null ? emp.DepartmentNavigation.DepartmentName + " " + emp.DepartmentNavigation.DepartmentCh : null,
+                Section = emp.SectionNavigation != null ? emp.SectionNavigation.SectionName + " " + emp.SectionNavigation.SectionCh : null,
+                Quota = emp.CodeQuotaNavigation?.Quotaa,
+
+                HazardDoc = _context.CompanyDocs.FirstOrDefault(d => d.Code == emp.HazardDoc)?.Title,
+                Sex = _context.Sexes.FirstOrDefault(s => s.CodeSex == emp.Sex)?.Sex,
+                Citizenship = _context.Citizenships.FirstOrDefault(c => c.CodeCitizenship == emp.Citizenship)?.Citizenship,
+                Nationality = _context.Nationalities.FirstOrDefault(n => n.CodeNationality == emp.Nationality)?.Nationality,
+                District = _context.Districts.FirstOrDefault(d => d.CodeDistrict == emp.District)?.District,
+
+                Photo = webPhotoPath
+            });
+        }
+
 
 
     }
