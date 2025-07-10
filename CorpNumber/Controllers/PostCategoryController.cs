4d5f4bee48a9ff93f78c0dfcfc7e10547578d188
Ahilless228 <alisherpro100gamer@gmail.com>
2025-07-10 11:18:57 +0600

    допилил стр Категории долж

--- CorpNumber/Controllers/PostCategoryController.cs
+++ CorpNumber/Controllers/PostCategoryController.cs
@@ -25,5 +25,48 @@ namespace CorpNumber.Controllers
                 .ToList();
             return View(model);
         }
+        //метод поиска по названию категории
+        [HttpGet]
+        public IActionResult FilterPostCategories(string searchText)
+        {
+            var query = _context.PostCategories.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                query = query.Where(c => c.Category!=null && c.Category.Contains(searchText));
+            }
+
+            var model = query.OrderBy(c => c.Category).ToList();
+            return PartialView("_PostCategoryTable", model);
+        }
+
+        [HttpGet]
+        public IActionResult GetPostCategoryById(int id)
+        {
+            var category = _context.PostCategories.FirstOrDefault(c => c.Code == id);
+            if (category == null) return NotFound();
+
+            return Json(new { code = category.Code, category = category.Category, categoryCh = category.CategoryCh });
+        }
+        //метод сохранения и обновления категории поста
+        [HttpPost]
+        public IActionResult SavePostCategory(PostCategory model)
+        {
+            if (model.Code == 0)
+            {
+                _context.PostCategories.Add(model);
+            }
+            else
+            {
+                var existing = _context.PostCategories.Find(model.Code);
+                if (existing == null) return NotFound();
+
+                existing.Category = model.Category;
+                existing.CategoryCh = model.CategoryCh;
+            }
+
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
