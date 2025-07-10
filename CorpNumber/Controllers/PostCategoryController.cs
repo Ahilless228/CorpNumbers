66757bd2436b1c739016932a58a13d82acf005ad
Ahilless228 <alisherpro100gamer@gmail.com>
2025-07-10 10:47:07 +0600

    добавил стр Категории долж

---
+++ CorpNumber/Controllers/PostCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CorpNumber.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace CorpNumber.Controllers
+{
+    public class PostCategoryController : Controller
+    {
+        private readonly CorpNumberDbContext _context;
+        public PostCategoryController(CorpNumberDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult PostCategoryIndex() 
+        {
+            var model= _context.PostCategories
+                .Select(c => new PostCategory
+                {
+                    Code = c.Code,
+                    Category = c.Category,
+                    CategoryCh = c.CategoryCh
+                })
+                .OrderBy(c => c.Category)
+                .ToList();
+            return View(model);
+        }
+    }
+}
